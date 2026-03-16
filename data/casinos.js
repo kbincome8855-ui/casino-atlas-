8d8af42bcd81b1b4a8a30a8e223f9a93809628bb
kbincome8855-ui <kbincome8855@gmail.com>
2026-03-15 23:11:30 -0500

    Create casinos.js

---
+++ data/casinos.js
@@ -0,0 +1,45 @@
+const casinos = [
+
+{
+name:"Chumba Casino",
+slug:"chumba-casino",
+type:"sweepstakes",
+bonus:"$10 Free Signup Bonus",
+min_withdrawal:100,
+withdrawal_speed:"24-48 hours",
+affiliate:"https://example.com/chumba"
+},
+
+{
+name:"Pulsz",
+slug:"pulsz",
+type:"sweepstakes",
+bonus:"$5 Free Signup Bonus",
+min_withdrawal:50,
+withdrawal_speed:"24-48 hours",
+affiliate:"https://example.com/pulsz"
+},
+
+{
+name:"Stake.us",
+slug:"stake-us",
+type:"sweepstakes",
+bonus:"$25 Free Signup Bonus",
+min_withdrawal:50,
+withdrawal_speed:"24 hours",
+affiliate:"https://example.com/stake"
+},
+
+{
+name:"High 5 Casino",
+slug:"high5-casino",
+type:"sweepstakes",
+bonus:"$5 Free Signup Bonus",
+min_withdrawal:50,
+withdrawal_speed:"48 hours",
+affiliate:"https://example.com/high5"
+}
+
+]
+
+module.exports = casinos
